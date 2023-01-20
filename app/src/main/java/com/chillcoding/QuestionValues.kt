8e492db077252b2ca2079c5847240231ec635a23
rugak <zianepromo@gmail.com>
2023-01-20 17:32:22 +0100

    .

---
+++ app/src/main/java/com/chillcoding/QuestionValues.kt
@@ -0,0 +1,4 @@
+package com.chillcoding
+
+object QuestionValues {
+}
---
+++ app/src/main/java/com/chillcoding/QuizActivity1.kt
@@ -0,0 +1,11 @@
+package com.chillcoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz1)
+    }
+}
