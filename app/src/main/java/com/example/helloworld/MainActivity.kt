dff5ac13b39283c8deeecdbdc70699104c866ad2
rugak <zianepromo@gmail.com>
2023-02-02 11:42:52 +0100

    menu score/restart/quit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.helloworld
 
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -13,6 +17,7 @@ private lateinit var trueButton: Button
 private lateinit var falseButton: Button
 private lateinit var nextButton: Button
 private lateinit var questionTextView: TextView
+
 private val questionBank= listOf(
     Question(R.string.question1, false),
     Question(R.string.question2, true),
@@ -39,6 +44,8 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
+
+
         fun checkAnswer(userAnswer: Boolean) {
             val correctAnswer = questionBank[currentIndex].correctAnswer
             val messageResId = if (userAnswer == correctAnswer) {
@@ -101,4 +108,22 @@ class MainActivity : AppCompatActivity() {
         Log.i(MainActivity::class.simpleName,
             getString(R.string.text_ondestroy))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val intent = intent
+        when (item.itemId){
+            R.id.menu_quit -> finish()
+
+            R.id.menu_score -> Toast.makeText(this,"No score yet",Toast.LENGTH_SHORT).show()
+
+            R.id.menu_restart -> {  val questionTextResId = questionBank[0].questionResId
+                                    questionTextView.setText(questionTextResId)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+}
 }
