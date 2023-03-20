5404d71a1aa2ca56ef887ee16ae8f0829111fcbd
rugak <zianepromo@gmail.com>
2023-03-20 21:37:31 +0100

    .

--- app/src/main/java/com/example/helloworld/HomeFragment.kt
+++ app/src/main/java/com/example/helloworld/HomeFragment.kt
@@ -1,18 +1,67 @@
 package com.example.helloworld
 
+import com.example.helloworld.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+lateinit var trueButton: Button
+lateinit var falseButton: Button
+lateinit var nextButton: Button
+lateinit var questionTextView: TextView
+
 class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+    private val questionBank = listOf(
+        Question(R.string.questionSport1, false),
+        Question(R.string.questionSport2, true),
+        Question(R.string.questionSport3, true),
+        Question(R.string.questionSport4, true),
+        Question(R.string.questionSport5, false),
+        Question(R.string.questionSport6, true),
+        Question(R.string.questionNature1, false),
+        Question(R.string.questionNature2, true),
+        Question(R.string.questionNature3, true),
+        Question(R.string.questionNature4, true),
+        Question(R.string.questionNature5, false),
+        Question(R.string.questionNature6, true),
+        Question(R.string.questionFood1, false),
+        Question(R.string.questionFood2, true),
+        Question(R.string.questionFood3, true),
+        Question(R.string.questionFood4, true),
+        Question(R.string.questionFood5, false),
+        Question(R.string.questionFood6, true),
+        Question(R.string.questionArt1, false),
+        Question(R.string.questionArt2, true),
+        Question(R.string.questionArt3, true),
+        Question(R.string.questionArt4, true),
+        Question(R.string.questionArt5, false),
+        Question(R.string.questionArt6, true),
+        Question(R.string.questionActuality1, false),
+        Question(R.string.questionActuality2, true),
+        Question(R.string.questionActuality3, true),
+        Question(R.string.questionActuality4, true),
+        Question(R.string.questionActuality5, false),
+        Question(R.string.questionActuality6, true),
+        Question(R.string.questionTek1, false),
+        Question(R.string.questionTek2, true),
+        Question(R.string.questionTek3, true),
+        Question(R.string.questionTek4, true),
+        Question(R.string.questionTek5, false),
+        Question(R.string.questionTek6, true)
+
+    )
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +75,50 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        trueButton = view.findViewById(R.id.true_button)
+        falseButton = view.findViewById(R.id.false_button)
+        nextButton = view.findViewById(R.id.next_button)
+        questionTextView = view.findViewById(R.id.question_text_view)
+
+        //TextView mName = (TextView)view.findViewById(R.id.myTextView);
+        //    mName.setText(firstName + " " + lastName);
+        //    return view;
+
+        fun checkAnswer(userAnswer: Boolean) {
+            val correctAnswer = questionBank[currentIndex].correctAnswer
+            val messageResId = if (userAnswer == correctAnswer) {
+                "Bonne réponse"
+            } else {
+                "Mauvaise réponse"
+            }
+            Toast.makeText(requireContext(), messageResId, Toast.LENGTH_SHORT).show()
+
+        }
+        trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+
+        }
+        falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+
+
+        }
+        val questionTextResId = questionBank[currentIndex].questionResId
+        questionTextView.setText(questionTextResId)
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].questionResId
+            questionTextView.setText(questionTextResId)
+        }
+        return view
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
     companion object {
 
         @JvmStatic
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.helloworld
 
-import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,11 +13,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.helloworld.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-private lateinit var trueButton: Button
-private lateinit var falseButton: Button
-private lateinit var nextButton: Button
-private lateinit var questionTextView: TextView
-lateinit var bottomNav: BottomNavigationView
+
 
 
 private val questionBank = listOf(
@@ -30,46 +22,31 @@ private val questionBank = listOf(
     Question(R.string.questionSport3, true),
     Question(R.string.questionSport4, true),
     Question(R.string.questionSport5, false),
-    Question(R.string.questionSport6, true)
-)
-
-private val questionBank2 = listOf(
+    Question(R.string.questionSport6, true),
     Question(R.string.questionNature1, false),
     Question(R.string.questionNature2, true),
     Question(R.string.questionNature3, true),
     Question(R.string.questionNature4, true),
     Question(R.string.questionNature5, false),
-    Question(R.string.questionNature6, true)
-)
-
-private val questionBank3 = listOf(
+    Question(R.string.questionNature6, true),
     Question(R.string.questionFood1, false),
     Question(R.string.questionFood2, true),
     Question(R.string.questionFood3, true),
     Question(R.string.questionFood4, true),
     Question(R.string.questionFood5, false),
-    Question(R.string.questionFood6, true)
-)
-
-private val questionBank4 = listOf(
+    Question(R.string.questionFood6, true),
     Question(R.string.questionArt1, false),
     Question(R.string.questionArt2, true),
     Question(R.string.questionArt3, true),
     Question(R.string.questionArt4, true),
     Question(R.string.questionArt5, false),
-    Question(R.string.questionArt6, true)
-)
-
-private val questionBank5 = listOf(
+    Question(R.string.questionArt6, true),
     Question(R.string.questionActuality1, false),
     Question(R.string.questionActuality2, true),
     Question(R.string.questionActuality3, true),
     Question(R.string.questionActuality4, true),
     Question(R.string.questionActuality5, false),
-    Question(R.string.questionActuality6, true)
-)
-
-private val questionBank6 = listOf(
+    Question(R.string.questionActuality6, true),
     Question(R.string.questionTek1, false),
     Question(R.string.questionTek2, true),
     Question(R.string.questionTek3, true),
@@ -95,52 +72,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        Log.i(
-            MainActivity::class.simpleName, getString(R.string.text_oncreate)
-        )
-
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
-        questionTextView = findViewById(R.id.question_text_view)
-
         val navView: BottomNavigationView = binding.bottomNav
         val navController = findNavController(R.id.nav_host_fragment_main)
         appBarConfiguration = AppBarConfiguration(
 
             setOf(
-                R.id.menu_home, R.id.menu_score, R.id.menu_settings
+                R.id.menu_home, R.id.menu_score, R.id.menu_theme
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController( navController)
 
-        fun checkAnswer(userAnswer: Boolean) {
-            val correctAnswer = questionBank[currentIndex].correctAnswer
-            val messageResId = if (userAnswer == correctAnswer) {
-                "Bonne réponse"
-            } else {
-                "Mauvaise réponse"
-            }
-            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
-        }
-        trueButton.setOnClickListener { view: View ->
-            checkAnswer(true)
-
-        }
-        falseButton.setOnClickListener { view: View ->
-            checkAnswer(false)
+        Log.i(
+            MainActivity::class.simpleName, getString(R.string.text_oncreate)
+        )
 
 
-        }
-        val questionTextResId = questionBank[currentIndex].questionResId
-        questionTextView.setText(questionTextResId)
-        nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].questionResId
-            questionTextView.setText(questionTextResId)
-        }
     }
 
 
